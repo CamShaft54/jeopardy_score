a8988f3b86a322161b4f1afabb8c6d3f7235eb99
camshaft54 <cam.shaw1234@gmail.com>
2021-01-02 20:47:04 -0800

    Add check for 0 players Add ability for client window to be kept on top

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+"""Run this file to start the program"""
 import PySimpleGUI as sg
 import sys
 
@@ -11,9 +12,11 @@ def start_setup_window():
         if event == 'Cancel' or event is None:  # if user closes window or clicks cancel
             setup_window.close()
             return "Cancelled"
-        if values[0].isnumeric():
+        elif values[0].isnumeric() and int(values[0]) > 0:
             setup_window.close()
             return int(values[0])
+        elif values[0] == "0":
+            sg.popup_error("There must be at least one player!")
 
 
 def start_enter_names_window(player_count):
@@ -31,31 +34,35 @@ def start_enter_names_window(player_count):
             return "Cancelled"
 
 
-def make_client_window(scores):
+def make_client_window(scores, keep_on_top):
     table_data = []
     for i in range(len(scores["players"])):
         row = [scores["players"][i], scores["scores"][i], scores["previous_scores"][i]]
         table_data.append(row)
-    client_layout = [[sg.Table(table_data, headings=["Players", "Scores", "Previous Scores"], key="score_table", hide_vertical_scroll=True,
+    client_layout = [[sg.Table(table_data, headings=["Players", "Scores", "Previous Scores"], key="score_table",
+                               hide_vertical_scroll=True,
                                num_rows=len(scores["players"]))]]
-    return sg.Window("Jeopardy Score Client", client_layout, finalize=True)
+    return sg.Window("Jeopardy Score Client", client_layout, finalize=True, keep_on_top=keep_on_top)
 
 
 def start_scorer_windows(players):
     scorer_layout = []
     for i in range(len(players)):
         scorer_layout.append([sg.Text(players[i] + ":"), sg.Input(key="player_{}".format(i + 1))])
-    scorer_layout.append([sg.Button("Update", key="update", bind_return_key=True), sg.Button("Start Client", key="client_start")])
+    scorer_layout.extend(
+        [[sg.Check("Keep client window on top", key="keep_on_top")],[sg.Button("Update", key="update", bind_return_key=True), sg.Button("Start Client", key="client_start")]])
     scorer_window = sg.Window("Jeopardy Score Host", scorer_layout, resizable=True, finalize=True)
     client_window, client_event, client_values = None, None, None
-    scores = {"players": players, "scores": ["0" for _ in range(len(players))], "previous_scores": ["0" for _ in range(len(players))]}
+    scores = {"players": players, "scores": ["0" for _ in range(len(players))],
+              "previous_scores": ["0" for _ in range(len(players))]}
     while True:
         scorer_event, scorer_values = scorer_window.read()
         if scorer_event is None:
             return
         elif scorer_event == "client_start" and not client_window:
-            scorer_window["client_start"].update(visible=False)
-            client_window = make_client_window(scores)
+            scorer_window["client_start"].update(disabled=True)
+            scorer_window["keep_on_top"].update(disabled=True)
+            client_window = make_client_window(scores, scorer_values["keep_on_top"])
         elif scorer_event == "update":
             scores["previous_scores"] = scores["scores"][:]
             for i in range(len(scores["scores"])):
@@ -63,7 +70,7 @@ def start_scorer_windows(players):
                 if add_to_score.isnumeric() or (
                         len(add_to_score) > 1 and add_to_score[0] == "-" and add_to_score[1:].isnumeric()):
                     scores["scores"][i] = str(int(scores["scores"][i]) + int(add_to_score))
-                scorer_window["player_" + str(i+1)]("")
+                scorer_window["player_" + str(i + 1)]("")
             if client_window is not None:
                 table_data = []
                 for i in range(len(players)):
